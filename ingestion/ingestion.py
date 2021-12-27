1b048da84e7380c75b31e62210e2a74f0b119285
areveny <areveny@protonmail.com>
2021-12-26 22:59:40 -0600

    Ingest lines, quests, place names

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -7,9 +7,21 @@ class Ingestion():
 
     def __init__(self, ingestion_db: ingestion_db.SqliteIngestionDB):
         self.ingestion_db = ingestion_db
+        # self.process_quests('raw-data/quest/*/*.csv')
+        self.process_quest_dir('raw-data/Quest.csv')
+        self.process_place_dir('raw-data/PlaceName.csv')
 
-    def get_quest_files(self):
-        files = glob.glob('raw-data/quest/*/*.csv')
+    def process_quests(self):
+        quest_files = self.get_quest_files()
+        processed = 0
+        for quest_file in quest_files:
+            self.process_file(quest_file)
+            processed = processed + 1
+            print('Ingested ({}/{}) quest CSVs into DB.'.format(processed, len(quest_files)), end='\r', flush=True)
+        print('\nDone processing individual quest texts.')
+
+    def get_quest_files(self, quest_glob_string):
+        files = glob.glob(quest_glob_string)
         files.sort()
 
         quest_files = list()
@@ -35,14 +47,44 @@ class Ingestion():
                     text_type, quest_label, quest_id, speaker, _, text_line = split_label
                     full_quest_id = '_'.join((quest_label.lower(), quest_id))
                     speaker = speaker.capitalize()
-                    data_rows.append((full_quest_id, speaker, text))
+                    if text:
+                        data_rows.append((full_quest_id, speaker, text))
                 elif len(split_label) == 5:
                     continue # Character response
         self.ingestion_db.upload_lines(data_rows)
 
+    def process_quest_dir(self, quest_dir_filename):
+        with open(quest_dir_filename) as f:
+            reader = csv.reader(f, delimiter=",", quotechar='"')
+            row = next(reader)
+            while row[0] != '65536':
+                row = next(reader)
+
+            quest_values = list()
+            for row in reader:
+                quest_numerical_id, quest_name, full_quest_id, expansion, class_job_category, quest_level = row[:6]
+                place_name = row[-11]
+                quest_values.append((full_quest_id.lower(), quest_name, quest_level, int(place_name)))
+        ingestion_db.upload_quests(quest_values)
+        print('Done processing quest lookup table.')
+
+    def process_place_dir(self, place_dir_filename):
+        with open(place_dir_filename) as f:
+            reader = csv.reader(f, delimiter=",", quotechar='"')
+            row = next(reader)
+            while row[0] != '0':
+                row = next(reader)
+            
+            place_values = list()
+            for row in reader:
+                place_id = row[0]
+                place_name = row[1]
+                if place_name:
+                    place_values.append((int(place_id), place_name))
+        ingestion_db.upload_places(place_values)
+        print('Done processing place lookup table.')
+
+
 if __name__ == '__main__':
-    ingestion_db = ingestion_db.SqliteIngestionDB('../query-service/ffxiv.db')
+    ingestion_db = ingestion_db.SqliteIngestionDB('../query-service/ffxiv.db', initialize=True)
     ingest = Ingestion(ingestion_db)
-    quest_files = ingest.get_quest_files()
-    for quest_file in quest_files:
-        ingest.process_file(quest_file)
--- ingestion/ingestion_db.py
+++ ingestion/ingestion_db.py
@@ -10,23 +10,52 @@ class IngestionDB():
 
 class SqliteIngestionDB(IngestionDB):
 
-    def __init__(self, db_file):
+    def __init__(self, db_file, initialize=True):
         self.db_file = db_file
-        self.initialize()
+        if initialize:
+            self.initialize()
 
     def initialize(self):
         con = sqlite3.connect(self.db_file)
-        with con:
-            con.execute("""DROP TABLE IF EXISTS lines;""")
-            con.execute("""CREATE TABLE lines (
-            quest_id TEXT,
-            speaker TEXT,
-            text TEXT
-            );
-            """)
-            con.execute("""CREATE INDEX quest_id ON lines (quest_id, speaker);""")
-            con.execute("""CREATE INDEX speaker ON lines (speaker, quest_id);""")
+        self.initialize_lines(con)
+        self.initialize_quests(con)
+        self.initialize_places(con)
         con.close()
+
+    def initialize_lines(self, con):
+        con.execute("""DROP TABLE IF EXISTS lines;""")
+        con.execute("""CREATE TABLE lines (
+        quest_id TEXT,
+        speaker TEXT,
+        text TEXT
+        );
+        """)
+        con.execute("""CREATE INDEX quest_id ON lines (quest_id, speaker);""")
+        con.execute("""CREATE INDEX speaker ON lines (speaker, quest_id);""")
+
+    def initialize_quests(self, con):
+        con.execute("""DROP TABLE IF EXISTS quests;""")
+        con.execute("""CREATE TABLE quests (
+        quest_id TEXT,
+        quest_name TEXT,
+        level INTEGER,
+        place_id INTEGER
+        );
+        """)
+        con.execute("""CREATE INDEX quest_id_quests ON quests (quest_id);""")
+        con.execute("""CREATE INDEX level ON quests (level, place_id);""")
+        con.execute("""CREATE INDEX place ON quests (place_id, level);""")
+
+    def initialize_places(self, con):
+        con.execute("""DROP TABLE IF EXISTS places;""")
+        con.execute("""CREATE TABLE places (
+        place_id INTEGER,
+        place_name TEXT
+        );
+        """)
+        con.execute("""CREATE INDEX place_id ON places (place_id);""")
+
+
 
     def upload_lines(self, db_values):
         con = sqlite3.connect(self.db_file)
@@ -34,6 +63,28 @@ class SqliteIngestionDB(IngestionDB):
             con.executemany('INSERT OR REPLACE INTO lines (quest_id, speaker, text) VALUES (?, ?, ?)', db_values)
         con.close()
 
+    def upload_quests(self, quest_values):
+        con = sqlite3.connect(self.db_file)
+        with con:
+            con.executemany('INSERT OR REPLACE INTO quests (quest_id, quest_name, level, place_id) VALUES (?, ?, ?, ?)', quest_values)
+        con.close()
+
+    def upload_places(self, place_values):
+        con = sqlite3.connect(self.db_file)
+        with con:
+            con.executemany('INSERT OR REPLACE INTO places (place_id, place_name) VALUES (?, ?)', place_values)
+        con.close()
+
+
 if __name__ == '__main__':
     con = sqlite3.connect('../query-service/ffxiv.db')
+
+    lines_by_quest_id = 'select * from lines WHERE quest_id="clshrv530_02049" ORDER BY rowid'
+    lines_by_speaker = 'select * from lines WHERE speaker="Lyse" ORDER BY rowid'
+    lines_by_speaker_and_text = 'select * from lines WHERE speaker="Lyse" AND TEXT like "%charging about%" ORDER BY rowid'
+    quests = 'select * from quests WHERE quest_id="clshrv530_02049" ORDER BY rowid'
+
+    join_query = 'select * from lines, quests WHERE lines.quest_id = quests.quest_id and quests.quest_id="clshrv530_02049" ORDER BY lines.rowid LIMIT 10'
+    results = con.execute(join_query).fetchall()
+    print(results)
     
